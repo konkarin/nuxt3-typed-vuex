b090367b82265be9c26a98949cf3b4e012367a41
konkarin <konkarin.photo@gmail.com>
2023-04-26 20:49:52 +0900

    refactor: 💡 Ensure compatibility with Nuxt v2

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
+import { NuxtApp } from "nuxt/dist/app/nuxt";
 import { accessor } from "./plugins/vuex";
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $accessor: typeof accessor;
+  }
+}
+
+// Nuxt v2との互換性のための型定義
+declare module "vuex/types/index" {
+  interface Store<S> {
+    app: NuxtApp;
   }
 }
--- plugins/vuex.ts
+++ plugins/vuex.ts
@@ -25,6 +25,8 @@ const actions = actionTree(
   { state, getters, mutations },
   {
     async resetEmail({ commit }) {
+      console.log(this.app.i18n);
+
       commit("setEmail", "a@a.com");
     },
   }
@@ -43,6 +45,10 @@ export const accessor = useAccessor(store, storePattern);
 const plugin: Plugin = defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(store);
   nuxtApp.vueApp.config.globalProperties.$accessor = accessor;
+
+  // Nuxt v2との互換性のためのcontext注入
+  // https://github.com/nuxt/nuxt/blob/d4b9e4b0553bcd617ecbc0b8b76871070b347fcb/packages/vue-app/template/index.js#L164-L165
+  store.app = nuxtApp.vueApp.$nuxt;
 
   return {
     provide: {
